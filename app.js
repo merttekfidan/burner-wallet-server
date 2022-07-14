b163624e30f702f451e480078cbf0fb7485e9ccb
Merdjan Tekfidan <merttekfidan@gmail.com>
2022-07-14 23:03:12 +0200

    cors

--- app.js
+++ app.js
@@ -6,7 +6,24 @@ const app = express()
 dotenv.config({ path: './config.env' });
 const API_KEY = process.env.ETHERSCAN_API
 app.use(cors())
-app.use('/api/v1',transactionRouter)
+//Cors Configuration - Start
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+    }
+    next()
+})
+//Cors Configuration - End
+app.use('/api/v1', transactionRouter)
 
 
 
